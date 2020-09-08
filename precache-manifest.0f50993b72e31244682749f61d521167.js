c8154c958234324331d4ffdb97e3703836121927
Karina Rosello <kr@Karinas-MBP.attlocal.net>
2020-09-08 15:59:28 -0400

    Updates

---
+++ precache-manifest.0f50993b72e31244682749f61d521167.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84b92fe922081dd26b583d8005eed2e2",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "2329fb44aa6f0bdbc904",
+    "url": "/my-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8fa05a725ce0020bc0c0",
+    "url": "/my-portfolio/static/js/2.4a91bcbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.4a91bcbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2329fb44aa6f0bdbc904",
+    "url": "/my-portfolio/static/js/main.05e832ff.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  }
+]);
